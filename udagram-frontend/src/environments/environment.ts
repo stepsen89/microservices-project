724e2ab85933bb631ff462838b10a6447c3ae458
Steph <stepsen.dev@gmail.com>
2022-01-18 13:43:24 +0000

    included screenshots

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a6fbdbef54eb24e3f8a61a6e7094fda3-1121925954.eu-west-2.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
